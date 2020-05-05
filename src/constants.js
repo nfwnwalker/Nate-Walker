fafd5ffbe57c2e9b923a2fb0ac1b97f7535d878c
nfwnwalker <61706922+nfwnwalker@users.noreply.github.com>
2020-05-05 11:19:28 -0400

    fixed bugs

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const primaryNavLinks = [
     { name: 'about', href: 'about' },
     { name: 'portfolio', href: 'portfolio' },
     { name: 'experience', href: 'experience' },
-    { name: 'contact', href: 'mailto:test@email.com' }
+    { name: 'contact', href: 'mailto:nfwnwalker@gmail.com' }
 ];
